a18a3701beba79809d1a6efe0f812992ab33c185
sdeepak09 <sdeepak@iitk.ac.in>
2017-02-11 20:31:02 +0530

    All done

--- Project_1_codes.R
+++ Project_1_codes.R
@@ -17,7 +17,8 @@ q1=function(n,r,true_a,true_b,prntile){
   est_coefs=list()
   mean_bias=list()
   mean_sqrd_error=list()
-  percentile_points=list()
+  percentile_points_for_a=list()
+  percentile_points_for_b=list()
   true_coef_mat=data.frame(true_a=c(rep(true_a,r)),true_b=c(rep(true_b,r)))
   true_coef_mat=as.matrix(true_coef_mat)
   for(i in 1:length(n)){
@@ -25,7 +26,8 @@ q1=function(n,r,true_a,true_b,prntile){
     est_coef_names=paste0("est_coef_for_n=",n[i])
     mean_bias_names=paste0("mean_bias_for_n=",n[i])
     mean_sqr_names=paste0("mean_sqrd_error_for_n=",n[i])
-    percnt_names=paste0("percentile_points_for_n=",n[i])
+    percnt_names_a=paste0("percentile_points_for_a_for_n=",n[i])
+    percnt_names_b=paste0("percentile_points_for_b_for_n=",n[i])
     for(j in 1:r){
       dsgn_mat_1=c(rep(1,n[i]))
       dsgn_mat_2=c(1:n[i])
@@ -43,11 +45,15 @@ q1=function(n,r,true_a,true_b,prntile){
     mean_bias[[mean_bias_names]]=mean_bias_vec
     mean_sqrd_error[[mean_sqr_names]]=mean_sqrd_vec
     vec_for_a=est_coef[,1]
-    ordrd_vec_for_a=order(vec_for_a)
-    perctl_vec=ordrd_vec_for_a[r*prntile]
-    percentile_points[[percnt_names]]=perctl_vec
+    ordrd_vec_for_a=sort(vec_for_a)
+    perctl_vec_for_a=ordrd_vec_for_a[c(r*prntile)]
+    percentile_points_for_a[[percnt_names_a]]=perctl_vec_for_a
+    vec_for_b=est_coef[,2]
+    ordrd_vec_for_b=sort(vec_for_b)
+    perctl_vec_for_b=ordrd_vec_for_b[c(r*prntile)]
+    percentile_points_for_b[[percnt_names_b]]=perctl_vec_for_b
   }
-  results=list(mean_bias=mean_bias,mean_sqrd_error=mean_sqrd_error,percentile_points=percentile_points)
+  results=list(est_coefs=est_coefs,mean_bias=mean_bias,mean_sqrd_error=mean_sqrd_error,percentile_points_for_a=percentile_points_for_a,percentile_points_for_b=percentile_points_for_b)
   return(results)
 }
 
